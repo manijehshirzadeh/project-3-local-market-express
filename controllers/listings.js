491a74cc365bac31c2731a7f49a35fa3524d720d
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-16 11:34:09 +1000

    Build a route that updates a single listing in the database and build a delete route that removes a single listing from the database by id.

--- controllers/listings.js
+++ controllers/listings.js
@@ -42,4 +42,45 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Update an existing listing
+router.put("/:id", async (req, res) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing.seller.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json("You are not permitted to modify this listing");
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    ).populate("seller");
+
+    res.json(updatedListing);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+});
+
+// Deleting a listing
+router.delete("/:id", async (req, res) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing.seller.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json("You are not permitted to delete this listing");
+    }
+
+    await listing.deleteOne();
+    res.json(listing);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+});
+
 module.exports = router;
